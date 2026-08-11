a19215d2a2e2716433cc4dc3126f8d1bc65c7dd3
Michael Dick <michaeldick000@gmail.com>
2026-08-10 22:53:58 -0400

    feat: completed 3310

--- src/graphs/3310_remove_methods_from_project.cpp
+++ src/graphs/3310_remove_methods_from_project.cpp
@@ -1,78 +1,72 @@
+#include <algorithm>
 #include <set>
 #include <stack>
 #include <vector>
 
-[[nodiscard]] std::vector<std::vector<int>> build_adj(const int n, const std::vector<std::vector<int>> &v)
+// build adj.
+
+// From adj do a dfs from problem node to the end. Make this a vector (lets call this a problem group).
+
+// for the remaining group, see if u is NOT part of the group and points to something in the group the 
+// removal is impossible.
+
+[[nodiscard]] std::vector<std::vector<int>> create_adj_list(const int n, std::vector<std::vector<int>>& v)
 {
-  std::vector<std::vector<int>> adj(n);
-  for (const std::vector<int> edges : v) {
-    const int u = edges[0], v = edges[1];
-    adj[u].push_back(v);
+  std::vector<std::vector<int>> adj_list(n);
+  for (const std::vector<int> edge : v) {
+    int u = edge[0], v = edge[1];
+    adj_list[u].push_back(v);
   }
-  return adj;
+
+  return adj_list;
 }
 
-std::set<int> group_dfs(const int n, const int k, const std::vector<std::vector<int>>& adj_list) {
-  std::set<int> group{};
-  std::stack<int> s{};
+[[nodiscard]] std::vector<bool> group_dfs(const int n, const int k, std::vector<std::vector<int>>& adj_list)
+{
+  std::stack<int> s;
+  // true at index if suspicious.
+  std::vector<bool> group(n, false);
   s.push(k);
-  group.insert(k);
+  group[k] = true;
+  // since this graph can be cyclic there must be a visted list to curb adding the same node twice.
   std::vector<bool> visited(n, false);
+  visited[k] = true;
   while (!s.empty()) {
     int curr_node = s.top();
     s.pop();
-    const std::vector<int> edges = adj_list[curr_node];
+    std::vector<int> edges = adj_list[curr_node];
     for (const int edge : edges) {
       if (!visited[edge]) {
-        s.push(edge);
-        group.insert(edge);
         visited[edge] = true;
+        group[edge] = true;
+        s.push(edge);
       }
     }
-  }
+  } 
   return group;
 }
 
-void dfs(int n, int node, std::vector<std::vector<int>>& adj_list, std::set<int>& group) {
-    std::stack<int> s{};
-    s.push(node);
-    int curr_node = -1;
-    std::vector<bool> visited(n, false);
-    while(!s.empty()) {
-        curr_node = s.top();
-        s.pop();
-        std::vector<int>& edges = adj_list[curr_node];
-        bool not_in_group = group.find(curr_node) == group.end();
-        for (const int edge : edges) {
-            if (not_in_group 
-                && group.find(edge) != group.end()) {
-                    group.clear();
-            }
-            if (!visited[edge]) {
-              s.push(edge);
-              visited[edge] = true;
-            }
-        }
-    }
-}
-
-using std::vector;
-vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) 
+std::vector<int> remainingMethods(int n, int k, std::vector<std::vector<int>>& invocations) 
 {
     std::vector<int> methods{};
-    auto adj_list = build_adj(n, invocations);
-    const auto v = adj_list[k];
-    std::set<int> group = group_dfs(n, k, adj_list);
-    for (int i = 0; i < n; ++i) {
-        dfs(n, i, adj_list, group);
+    methods.reserve(n);
+    std::vector<std::vector<int>> adj_list = create_adj_list(n, invocations);
+    std::vector<bool> group = group_dfs(n, k, adj_list);
+    bool can_remove = true;
+    for (const auto& edge : invocations) {
+      int u = edge[0], v = edge[1];
+      if (!group[u] && group[v]) {
+        can_remove = false;
+        break;
+      }
     }
-    std::vector<int> result;
     for (int i = 0; i < n; ++i) {
-        if (group.find(i) == group.end()) {
-            result.push_back(i);
-        }
+      if (can_remove && group[i]) {
+        continue;
+      }
+      methods.push_back(i);
     }
-    return result;
+    return methods;
 }
 
 int main()
@@ -80,21 +74,21 @@ int main()
   using std::vector;
 
   // Test 1
-  //   vector<vector<int>> invocations = {
-  //   {1, 2 },
-  //   {0, 1 },
-  //   {3, 2 },
-  // };
-  // int n = 4, k = 1;
-
-  // Test 2
     vector<vector<int>> invocations = {
     {1, 2 },
-    {0, 2 },
     {0, 1 },
-    {3, 4 },
+    {3, 2 },
   };
-  int n = 5, k = 0;
+  int n = 4, k = 1;
+
+  // Test 2
+  //   vector<vector<int>> invocations = {
+  //   {1, 2 },
+  //   {0, 2 },
+  //   {0, 1 },
+  //   {3, 4 },
+  // };
+  // int n = 5, k = 0;
   // Test 3
   //   vector<vector<int>> invocations = {
   //   {1, 2 },
